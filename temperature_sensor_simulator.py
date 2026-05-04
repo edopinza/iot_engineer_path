ee73815fbb68dca37ab0c84f171aaece8d5f2c9a
edopinza <edoardo.pinzaferri@gmail.com>
2026-05-04 15:10:30 +0200

    Piccole modifiche

--- temperature_sensor_simulator.py
+++ temperature_sensor_simulator.py
@@ -12,8 +12,9 @@ class SmartSensor:
         self.current_time = None
 
     def record_temperature(self):
+        # Registra una temperatura associata al timestamp. Il valore è generato randomicamente.
         self.current_time = datetime.now().timestamp()
-        self.current_temperature = random.uniform(36.5, 37.5)
+        self.current_temperature = round(random.uniform(36.5, 37.5), 2)
 
         if len(self.records) > 10:
             del self.records[0]
@@ -21,12 +22,14 @@ class SmartSensor:
         self.records.append({self.current_time: self.current_temperature})
 
     def alert(self):
+        # Stampa su terminale un alert se la temperatura va oltre il limite minimo o massimo
         if self.current_temperature < self._lower_limit:
             print("WARNING: temperature is too low!")
         elif self.current_temperature > self._upper_limit:
             print("WARNING: temperature is too high!")
 
     def get_json_payload(self):
+        # Converte 
         payload_dict = {
             "timestamp": list(self.records[0].keys())[0],
             "temperature": list(self.records[0].values())[0],
@@ -40,3 +43,6 @@ class SmartSensor:
 incubator_sensor = SmartSensor(upper_limit=37.2, lower_limit=36.8)
 incubator_sensor.record_temperature()
 incubator_sensor.alert()
+
+payload = incubator_sensor.get_json_payload()
+print(f"Payload pronto per la rete: {payload}")
